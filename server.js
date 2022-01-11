14a6fe4c4f384436da35c0d7df0bc5486c6c8053
leticianardi <leticia.tnardi@gmail.com>
2022-01-11 00:15:07 -0800

    add menu questions

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mysql = require('mysql2');
 // const connection = require('./connection');
 
 // Connect to database
-const connection = mysql.createConnection(
+const database = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
@@ -16,3 +16,62 @@ const connection = mysql.createConnection(
   },
   console.log(`Connected to employees database.`)
 );
+
+// opening questions
+function menu() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'menuQuestions',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'Add department',
+          'View roles',
+          'Add role',
+          'View employess',
+          'Add employee',
+          'Update employee',
+          'Exit'
+        ]
+      }
+    ])
+    .then(function (answers) {
+      switch (answers.menuQuestions) {
+        case 'View all departments':
+          allDepartments();
+          break;
+
+        case 'Add department':
+          addDepartment();
+          break;
+
+        case 'View roles':
+          allRoles();
+          break;
+
+        case 'Add role':
+          addRole();
+          break;
+
+        case 'View employess':
+          allEmployees();
+          break;
+
+        case 'Add employee':
+          addEmployee();
+          break;
+
+        case 'Update employee':
+          updateEmployee();
+          break;
+
+        case 'Exit':
+          database.end();
+          break;
+      }
+    });
+}
+
+menu();
